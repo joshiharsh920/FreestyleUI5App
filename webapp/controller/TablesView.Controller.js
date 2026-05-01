8897fd59914a984e84e8c775ce4dc2a29401b842
Harsh <joshiharsh920@gmail.com>
2026-05-01 18:06:33 +0530

    added code

---
+++ webapp/controller/TablesView.Controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
+    "sap/m/MessageBox",
+    "sap/m/MessageToast"
+], (Controller, Filter, FilterOperator, MessageBox, MessageToast) => {
+    "use strict";
+
+    return Controller.extend("joshi.project1trial.controller.TablesView", {
